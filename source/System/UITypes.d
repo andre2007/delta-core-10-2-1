249a1632cb1571347086743e764113a88dd5c2d5
andre2007 <andre@s-e-a-p.de>
2018-08-11 21:53:07 +0200

    Deprecation built-in hex string

--- source/System/UITypes.d
+++ source/System/UITypes.d
@@ -1,6 +1,6 @@
 module System.UITypes;
 
-import std.conv: parse;
+import std.conv: parse, hexString;
 
 // alias TAlphaColor = uint;
 struct TAlphaColor
@@ -24,12 +24,12 @@ alias TAlphaColors = TAlphaColorRec;
 
 struct TAlphaColorRec
 {
-	enum Alpha = TAlphaColor(x"FF000000");
-	enum Black = TAlphaColor(x"00000000");
-	enum Ivory = TAlphaColor(x"FFFFFFF0");
+	enum Alpha = TAlphaColor(hexString!"FF000000");
+	enum Black = TAlphaColor(hexString!"00000000");
+	enum Ivory = TAlphaColor(hexString!"FFFFFFF0");
 	
 	
-	enum White = TAlphaColorRec.Alpha | TAlphaColor(x"FFFFFF");
+	enum White = TAlphaColorRec.Alpha | TAlphaColor(hexString!"FFFFFF");
 }
 
 alias TFontStyles = TFontStyle[];
